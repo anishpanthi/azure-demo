9402aab01bdb9f94ecce62941bd0215d06f5b503
Panthi <t158909@hms.com>
2019-08-01 09:24:13 -0500

    Initial Commit

---
+++ src/main/java/com/hms/azure/cloud/poc/CloudPocApplication.java
@@ -0,0 +1,13 @@
+package com.hms.azure.cloud.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudPocApplication.class, args);
+    }
+
+}
